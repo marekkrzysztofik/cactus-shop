c33c1fcf8167aa7fa925134ed25e6e7bd0ef34e3
marekkrzysztofik <marekkrzysztofik2@wp.pl>
2023-05-19 20:18:25 +0200

    styles

--- src/App.vue
+++ src/App.vue
@@ -48,7 +48,7 @@
   </div>
   <div class="margin-top-5"><RouterView /></div>
   <footer class="footer flex">
-    <div class="footer__box flex column">
+    <div class="footer__box flex column area-1">
       <h1 class="heading-primary">Kontakt</h1>
       <div class="flex">
         <i class="pi pi-map-marker" />
@@ -59,7 +59,7 @@
         <p>123456789</p>
       </div>
     </div>
-    <div class="footer__box">
+    <div class="footer__box area-2">
       <p>Regulamin</p>
       <p>Polityka prywatności</p>
       <p>Zamówienia</p>
@@ -68,7 +68,7 @@
       <p>Ochrona Danych Osobowych</p>
       <p>Metody płatności</p>
     </div>
-    <div class="footer__box">
+    <div class="footer__box area-3">
       <h1 class="heading-primary">tu ci kaktus wyrośnie</h1>
       <div class="flex">
         <i class="pi pi-instagram" />
