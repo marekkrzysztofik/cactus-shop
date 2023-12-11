8bf4d6d5e54f0a66e5d399c15ef7090469ede206
marekkrzysztofik <marekkrzysztofik2@wp.pl>
2023-12-11 16:18:28 +0100

    src change

--- src/consts/products.ts
+++ src/consts/products.ts
@@ -9,25 +9,25 @@ export const allProducts: Array<Product> = [
   {
     name: 'ZAMIOCULCAS ZAMIOFOLIA',
     price: 24,
-    image: '/images/kaktus1.webp',
+    image: 'https://zielony-parapet.pl/36087-thickbox_default/opuntia-microdasys-albata-opuncja.jpg',
     quantity: 1
   },
   {
     name: 'SANSEWERIA SANSEVIERIA',
     price: 28,
-    image: '/images/kaktus3.webp',
+    image: 'https://zielony-parapet.pl/17252-thickbox_default/zamioculcas-zamiifolia-supernova-zamiokulkas-zamiolistny.jpg',
     quantity: 1
   },
   {
     name: 'KAKTUS OPUNCJA',
     price: 28,
-    image: '/images/kaktus2.webp',
+    image: 'https://zielony-parapet.pl/36087-thickbox_default/opuntia-microdasys-albata-opuncja.jpg',
     quantity: 1
   },
   {
     name: 'ZAMIOCULCAS ZAMIOFOLIA',
     price: 24,
-    image: '/images/kaktus1.webp',
+    image: 'https://zielony-parapet.pl/17252-thickbox_default/zamioculcas-zamiifolia-supernova-zamiokulkas-zamiolistny.jpg',
     quantity: 1
   }
 ]
